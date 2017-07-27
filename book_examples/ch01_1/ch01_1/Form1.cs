25393d543c6bea4e750712b2a2f7777d12d4a6d3
Johan Kämpe <kampe.johan@gmail.com>
2017-07-27 11:30:05 +0200

    Book Example 1.1
    Button and MessageBox

---
+++ book_examples/ch01_1/ch01_1/Form1.cs
@@ -0,0 +1,31 @@
+﻿/* Programmering 2 med C#
+ * Example 1.1
+ * Page 10
+ * 
+ * Windows Forms with button and message box
+ * 
+ * Johan Kämpe
+ * https://github.com/GoblinDynamiteer/
+ * https://www.linkedin.com/in/johankampe/
+ * 
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace ch01_1
+{
+    public partial class Form1 : Form // Form1 inherits Form
+    {
+        public Form1() // Consructor
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("MessageBox text that shows every " +
+                "time the button is clicked!", "MessageBox Title");
+        }
+    }
+}
