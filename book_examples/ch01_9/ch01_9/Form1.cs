e0bd6fa6436f7fd5867cad52f3a8f54fa551ff4c
Johan Kämpe <kampe.johan@gmail.com>
2017-08-01 13:18:07 +0200

    Book Example 1.9
    Listboxes

---
+++ book_examples/ch01_9/ch01_9/Form1.cs
@@ -0,0 +1,39 @@
+﻿/* Programmering 2 med C#
+ * Example 1.9
+ * Page 45
+ * 
+ * ListBoxes
+ * 
+ * Johan Kämpe
+ * https://github.com/GoblinDynamiteer/
+ * https://www.linkedin.com/in/johankampe/
+ * 
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace ch01_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* Move selected item from listBox1 to listBox2 */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(listBox1.Text);
+            listBox1.Items.Remove(listBox1.Text);
+        }
+
+        /* Move selected item from listBox2 to listBox1 */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(listBox2.Text);
+            listBox2.Items.Remove(listBox2.Text);
+        }
+    }
+}
