06149456dc6ec391c921751e064fce2a0d2fdae0
Johan Kämpe <kampe.johan@gmail.com>
2017-08-01 18:50:59 +0200

    Book Exercise 4.1

---
+++ book_exercises/ch4_01/ch4_01/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Programmering 2 med C#
+ * Exercise 4.1
+ * Page 174
+ * 
+ * Create a class: Rectangle with private members
+ * width, height and methods Area() and 
+ * Circumference().
+ * Add a constructor with same parameter names
+ * as the members.
+ * 
+ * Create a rectangle object in the Main class,
+ * print the area and circumference
+ * 
+ * Johan Kämpe
+ * https://github.com/GoblinDynamiteer/
+ * https://www.linkedin.com/in/johankampe/
+ * 
+ */
+
+using System;
+
+namespace ch4_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(22.1, 33.2);
+
+            Console.WriteLine("Area: " + rect.Area());
+            Console.WriteLine("Circumference: " + rect.Circumference());
+
+            Console.ReadLine();
+        }
+    }
+
+    class Rectangle
+    {
+        private double width, height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Circumference()
+        {
+            return width * 2 + height * 2;
+        }
+
+        public double Area()
+        {
+            return width * height;
+        }
+    }
+}
