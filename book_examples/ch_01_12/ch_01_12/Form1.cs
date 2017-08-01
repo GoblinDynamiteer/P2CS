eb8c5cec7baaca8ebc46a87f1e4888923a8b8e30
Johan Kämpe <kampe.johan@gmail.com>
2017-08-01 15:06:20 +0200

    Book Example 1.12
    MultiDocumentWindow

---
+++ book_examples/ch_01_12/ch_01_12/Form1.cs
@@ -0,0 +1,73 @@
+﻿/* Programmering 2 med C#
+ * Example 1.12
+ * Page 53
+ * 
+ * Multiple Document Interface
+ * 
+ * Johan Kämpe
+ * https://github.com/GoblinDynamiteer/
+ * https://www.linkedin.com/in/johankampe/
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch_01_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void child1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            childForm child = new childForm("First Image", "\\Welcome.jpg");
+            child.MdiParent = this;
+            child.Show();
+        }
+
+        private void child2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            childForm child = new childForm("Second Image", "\\Welcome.jpg");
+            child.MdiParent = this;
+            child.Show();
+        }
+
+        private void child3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            childForm child = new childForm("Third Image", "\\Welcome.jpg");
+            child.MdiParent = this;
+            child.Show();
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void tileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void tileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+    }
+}
---
+++ book_examples/ch_01_12/ch_01_12/childForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ch_01_12
+{
+    public partial class childForm : Form
+    {
+        public childForm(string title, string fileName)
+        {
+            InitializeComponent();
+            Text = title;
+            pictureBox1.Image = Image.FromFile(
+                Directory.GetCurrentDirectory() + fileName);
+        }
+    }
+}
