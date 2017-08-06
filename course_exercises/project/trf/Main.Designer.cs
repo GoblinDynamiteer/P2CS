9c8771ed564abcd587615dafb6540e830931f5c4
Johan Kämpe <kampe.johan@gmail.com>
2017-08-06 11:30:54 +0200

    Code

---
+++ course_exercises/project/trf/Main.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace trf
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lägg till medlem";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 809);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "TRF - Tigerälskarnas Riksförbund";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
