cc04f7d9814810ff537177d2656c84f21436c526
Johan Kämpe <kampe.johan@gmail.com>
2017-08-08 15:31:09 +0200

    Book Example 6.5
    SQL Database

---
+++ book_examples/ch06_05/ch06_05/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch06_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void titlesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.titlesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.böckerDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'böckerDataSet.Titles' table. You can move, or remove it, as needed.
+            this.titlesTableAdapter.Fill(this.böckerDataSet.Titles);
+
+        }
+    }
+}
