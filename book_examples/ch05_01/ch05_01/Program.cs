662ef3372b86ad93d6e8880833908ce9a6ff2a3b
Johan Kämpe <kampe.johan@gmail.com>
2017-08-08 13:23:14 +0200

    Book Example 5.1
    File write/read with StreamWriter

---
+++ book_examples/ch05_01/ch05_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ch05_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            StreamWriter fileForWrite = new StreamWriter("WriteRead.txt");
+
+            fileForWrite.WriteLine("\n\tTextText");
+            fileForWrite.Close();
+
+            StreamReader fileForRead = new StreamReader("WriteRead.txt");
+
+            Console.WriteLine("From textfile: ");
+            while (!fileForRead.EndOfStream)
+            {
+                word = fileForRead.ReadLine();
+                Console.WriteLine(word);
+            }
+
+            fileForRead.Close();
+            Console.ReadKey();
+        
+        }
+    }
+}
